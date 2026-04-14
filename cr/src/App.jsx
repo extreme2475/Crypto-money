56b9a012096cd21c3212dd1cad9840cfb31c7fbd
Himanshu <rawanihimanshu15@gmail.com>
2026-04-14 16:15:45 +0530

    Final SMTP fix

--- cr/src/App.jsx
+++ cr/src/App.jsx
@@ -4,8 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Coin from './pages/Home/Coin/Coin';
 import Footer from './components/Footer/footer';
-
-
+import Advisor from './pages/Home/Advisor/Advisor'; // Already imported, now we use it
 
 const App = () => {
   return (
@@ -14,6 +13,8 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/coin/:coinId' element={<Coin />} />
+        {/* Added the route for Advisor below */}
+        <Route path='/advisor' element={<Advisor />} />
       </Routes>
       <Footer/>
     </div>
--- cr/src/components/Navbar/Navbar.jsx
+++ cr/src/components/Navbar/Navbar.jsx
@@ -6,60 +6,61 @@ import { Coincontext } from '../../context/Coincontext'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-  const {setCurrency} = useContext(Coincontext)
+  const { setCurrency } = useContext(Coincontext)
 
-  const currencyHandler=(event)=>{
-switch (event.target.value){
-  case "usd":{
-    setCurrency({name:"usd" ,symbol:"$"})
-    break;
+  const currencyHandler = (event) => {
+    switch (event.target.value) {
+      case "usd": {
+        setCurrency({ name: "usd", symbol: "$" })
+        break;
+      }
+      case "eur": {
+        setCurrency({ name: "eur", symbol: "€" })
+        break;
+      }
+      case "inr": {
+        setCurrency({ name: "inr", symbol: "₹" })
+        break;
+      }
+      default: {
+        setCurrency({ name: "inr", symbol: "₹" })
+        break;
+      }
+    }
   }
-  case "eur":{
-    setCurrency({name:"eur" ,symbol:"€"})
-    break;
-  }
-  case "inr":{
-    setCurrency({name:"inr" ,symbol:"₹"})
-    break;
-  }
-  default:{
-    setCurrency({name:"inr" ,symbol:"₹"})
-    break;
-  }
-}
-  }
+
   return (
     <div className='navbar'>
-<Link to={'/'}>
-      <img src={logo} alt="" className='logo'/>
+      <Link to={'/'}>
+        <img src={logo} alt="" className='logo' />
       </Link>
 
       <ul>
         <Link to={'/'}><li>Home</li></Link>
-        
+
         <li>
-  <a href="https://www.coindesk.com/" target="_blank" rel="noopener noreferrer">
-    Blog
-  </a>
-</li>
-<li>
-  <a href="https://www.moneycontrol.com/stocksmarketsindia/" target="_blank" rel="noopener noreferrer">
-     Stock Market
-  </a>
-</li>
-<li>
-  <a>AI Advisor</a>
-</li>
+          <a href="https://www.coindesk.com/" target="_blank" rel="noopener noreferrer">
+            Blog
+          </a>
+        </li>
+        <li>
+          <a href="https://www.moneycontrol.com/stocksmarketsindia/" target="_blank" rel="noopener noreferrer">
+            Stock Market
+          </a>
+        </li>
+        <li>
+          {/* Fixed: Use Link instead of <a> for internal routing */}
+          <Link to={'/advisor'}><li>AI Advisor</li></Link>
+        </li>
 
       </ul>
       <div className='nav-right'>
-        <select onChange={ currencyHandler}>
-            <option value="usd">USD</option>
-            <option value="eur">EUR</option>
-            <option value="inr">INR</option>
+        <select onChange={currencyHandler}>
+          <option value="usd">USD</option>
+          <option value="eur">EUR</option>
+          <option value="inr">INR</option>
         </select>
-        <button>Sign up <img src={arrow_icom} alt=""/></button>
-
+        <button>Sign up <img src={arrow_icom} alt="" /></button>
       </div>
     </div>
   )
